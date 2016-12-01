5d54134e79fe48339f8ff16a03fc66fac0580a7e
Jefferson <jeff.kmt@gmail.com>
2016-12-01 01:15:06 -0200

    3 cenarios

--- Sistema/DOS_ataque.java
+++ Sistema/DOS_ataque.java
@@ -7,36 +7,49 @@ public class DOS_ataque{
 
     public static void main(String args[]) throws Exception{
 		double bitrate, entropia, hurst, tc;
+		boolean traffic;
+
+        System.out.println("O sistema ira ler 3 logs diferentes, e imprimir os resultados na saida padrão e nos arquivos de siada .csv, cada um correspondente aos cenarios descritos no artigo");            
         
         String fileName = "fcl/dos_attack.fcl";
         FIS fis = FIS.load(fileName, true);
         JDialogFis jdf = new JDialogFis(fis, 800, 600);
-        
-		Escritor saida=new Escritor("files/saida.csv");
-		Scanner scan=new Scanner(new File("files/entrada.txt"));
 
-		while(scan.hasNext()){
-			bitrate=Double.parseDouble(scan.next());
-			entropia=Double.parseDouble(scan.next());
-			hurst=Double.parseDouble(scan.next());
+		String s="files/saida", entrada="files/entrada";
 
-            // Evaluate system using these parameters
-            fis.getVariable("hst").setValue(hurst);
-            fis.getVariable("ent").setValue(entropia);
-            fis.getVariable("brt").setValue(bitrate);
-            fis.evaluate();
+		for(int i=1; i<4; i++){
+			Escritor saida=new Escritor(s+i+".csv");
+			Scanner scan=new Scanner(new File(entrada+i+".txt"));
+			System.out.println(i);
 
-            tc = fis.getVariable("tc").getValue();
-            
-            // Print result & update plot
-            System.out.println("Bitrate: " + bitrate + " Entropia: " + entropia + " Hurst: " + hurst + " Classe: " + tc);
-            jdf.repaint();
+			while(scan.hasNext()){
+				bitrate=Double.parseDouble(scan.next());
+				entropia=Double.parseDouble(scan.next());
+				hurst=Double.parseDouble(scan.next());
 
-			saida.write(bitrate+"", entropia+"", hurst+"", tc+"");
+				// Evaluate system using these parameters
+				fis.getVariable("hst").setValue(hurst);
+				fis.getVariable("ent").setValue(entropia);
+				fis.getVariable("brt").setValue(bitrate);
+				fis.evaluate();
 
-			Thread.sleep(1000);
+				tc = fis.getVariable("tc").getValue();
+				
+				// Print result & update plot
+				System.out.print("Bitrate: " + bitrate + " Entropia: " + entropia + " Hurst: " + hurst + " Classe: " + tc);
+				if(tc>0.5)
+					System.out.println(" Ataque");
+				else
+					System.out.println(" Normal");
+
+				jdf.repaint();
+
+				saida.write(bitrate+"", entropia+"", hurst+"", tc+"");
+
+				Thread.sleep(2000);
+			}
+			saida.close();
 		}
-		saida.close();
         System.out.println("Final da execução do loop da geração dos valores do controlador fuzzy.");            
     }
 }
